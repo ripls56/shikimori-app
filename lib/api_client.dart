4a9b29ae745fe800d96ff66230cb4cdf7a8f43bc
ripls56 <kanada.smirnov@gmail.com>
2023-01-19 21:34:49 +0300

    authentication done

---
+++ lib/api_client.dart
@@ -0,0 +1,117 @@
+import 'package:dio/dio.dart';
+import 'package:oauth2/oauth2.dart' as oauth2;
+import 'package:oauth2/oauth2.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:shikimori_app/auth.dart';
+
+class ApiClient {
+  late AuthorizationCodeGrant grant;
+
+  late Uri authorizationUrl;
+
+  ApiClient() {
+    grant = oauth2.AuthorizationCodeGrant(
+        identifier, authorizationEndpoint, tokenEndpoint,
+        secret: secret);
+    authorizationUrl = grant.getAuthorizationUrl(_redirectUri,
+        scopes: ['user_rates', 'comments', 'topics']);
+  }
+
+  static const _host = 'https://shikimori.one/api';
+  static const _hostV2 = 'https://shikimori.one/api/v2';
+
+  final _redirectUri = Uri.parse('urn:ietf:wg:oauth:2.0:oob');
+
+  final authorizationEndpoint =
+      Uri.parse('https://shikimori.one/oauth/authorize');
+  final tokenEndpoint = Uri.parse('https://shikimori.one/oauth/authorize/');
+
+  static const identifier = 'jWeRpE8bKQ6eT3fTw2yDYS3hup04Zx5v4CMJ9hMqDk4';
+  static const secret = 'jwKxAkVNoOvJqcgro1dsAndXQr0ijJAwxWlNy-ML-ic';
+
+  String code = "";
+
+  Future<String> getAccessToken() async {
+    //var a = authorizationUrl.queryParameters;
+    Dio dio = Dio();
+    dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true,
+        responseHeader: false,
+        error: true,
+        compact: true,
+        maxWidth: 90));
+    var response =
+        await dio.post('https://shikimori.one/oauth/token', queryParameters: {
+      'grant_type': 'authorization_code',
+      'client_id': identifier,
+      'client_secret': secret,
+      'code': code,
+      'redirect_uri': _redirectUri.toString()
+    });
+    final String token = Auth.fromJson(response.data).accessToken;
+    return token;
+  }
+
+  String accessToken = '';
+
+  Future<void> getCreditionals() async {
+    Dio dio = Dio();
+    dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true,
+        responseHeader: false,
+        error: true,
+        compact: true,
+        maxWidth: 90));
+    getAccessToken().then((value) async => {
+          accessToken = value,
+          await dio.get(
+            '$_host/users/whoami',
+            options: Options(
+              headers: {
+                'User-Agent': 'mpt coursework',
+                'Authorization': 'Bearer $value'
+              },
+            ),
+          ),
+        });
+  }
+
+  Future<void> getRateList() async {
+    Dio dio = Dio();
+    dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true,
+        responseHeader: false,
+        error: true,
+        compact: true,
+        maxWidth: 90));
+    await dio.post(
+      '$_hostV2/user_rates/',
+      data: {
+        "user_rate": {
+          "chapters": "4",
+          "episodes": "2",
+          "rewatches": "5",
+          "score": "10",
+          "status": "completed",
+          "target_id": "15",
+          "target_type": "Anime",
+          "text": "test",
+          "user_id": "23456789",
+          "volumes": "3"
+        }
+      },
+      options: Options(
+        headers: {
+          'User-Agent': 'mpt coursework',
+          'Authorization': 'Bearer $accessToken'
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,49 +4,26 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:shikimori_app/api_client.dart';
 import 'package:uni_links/uni_links.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:oauth2/oauth2.dart' as oauth2;
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    StreamSubscription _sub;
-
-    Future<void> initUniLinks() async {
-      // ... check initialLink
-      print('D/my statred');
-      // Attach a listener to the stream
-      _sub = linkStream.listen((link) {
-        print('D/my work');
-        print('D/my $link');
-      });
-      // NOTE: Don't forget to call _sub.cancel() in dispose
-    }
-
-    initUniLinks();
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Home());
-  }
-
-  final Uri _url = Uri.parse(
-      'https://shikimori.one/oauth/authorize?client_id=bce7ad35b631293ff006be882496b29171792c8839b5094115268da7a97ca34c&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&scope=user_rates');
-
-  Future<void> _launchUrl() async {
-    if (!await launchUrl(_url)) {
-      throw 'Could not launch $_url';
-    }
+        home: const Home());
   }
 }
 
@@ -55,47 +32,7 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final redirectUri = Uri.parse('urn:ietf:wg:oauth:2.0:oob');
-
-    final authorizationEndpoint =
-        Uri.parse('https://shikimori.one/oauth/authorize');
-    final tokenEndpoint = Uri.parse('https://shikimori.one/oauth/authorize/');
-
-    const identifier =
-        'bce7ad35b631293ff006be882496b29171792c8839b5094115268da7a97ca34c';
-    const secret =
-        '811459eada36b14ff0cf0cc353f8162e72a7d6e6c7930b647a5c587d1beffe68';
-
-    String code = "";
-
-    var grant = oauth2.AuthorizationCodeGrant(
-        identifier, authorizationEndpoint, tokenEndpoint,
-        secret: secret);
-    var authorizationUrl =
-        grant.getAuthorizationUrl(redirectUri, scopes: ['user_rates']);
-    var a = authorizationUrl.queryParameters;
-
-    Future<void> getAccessToken() async {
-      Dio _dio = Dio();
-      _dio.interceptors.add(PrettyDioLogger(
-          requestHeader: true,
-          requestBody: true,
-          responseBody: true,
-          responseHeader: false,
-          error: true,
-          compact: true,
-          maxWidth: 90));
-      var response = await _dio
-          .post('https://shikimori.one/oauth/token', queryParameters: {
-        'grant_type': 'authorization_code',
-        'client_id': identifier,
-        'client_secret': secret,
-        'code': code,
-        'redirect_uri': redirectUri.toString()
-      });
-      await _dio.get('https://shikimori.one/api/users/whoami');
-    }
-
+    ApiClient apiClient = ApiClient();
     final controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
@@ -106,25 +43,32 @@ class Home extends StatelessWidget {
           },
           onPageStarted: (String url) {},
           onPageFinished: (String url) async {
-            if (url.startsWith(tokenEndpoint.toString())) {
-              getAccessToken();
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: ((context) =>
-                      Scaffold(body: Center(child: Text('work'))))));
-              code = url.split('/')[5];
-              print(code);
+            if (url.startsWith(apiClient.tokenEndpoint.toString())) {
+              if (apiClient.code != "") apiClient.getCreditionals();
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: ((context) => Scaffold(
+                        floatingActionButton: FloatingActionButton(
+                            onPressed: () => apiClient.getRateList()),
+                        body: Center(
+                          child: Text('work'),
+                        ),
+                      )),
+                ),
+              ); // home window
+              apiClient.code = url.split('/')[5];
             }
           },
           onWebResourceError: (WebResourceError error) {},
           onNavigationRequest: (NavigationRequest request) async {
-            if (request.url.contains(tokenEndpoint.toString())) {
+            if (request.url.contains(apiClient.tokenEndpoint.toString())) {
               return NavigationDecision.navigate;
             }
             return NavigationDecision.navigate;
           },
         ),
       )
-      ..loadRequest(authorizationUrl);
+      ..loadRequest(apiClient.authorizationUrl);
 
     return SafeArea(
       child: Scaffold(
